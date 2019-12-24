774fe9e9ed16f018581efb583bea3707cc83185b
X <evilbunny.x@gmail.com>
2019-12-24 18:07:47 +0300

    Telegram bot

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/vasyahuyasa/reviewboss
 
 go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
+)
--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"errors"
 	"log"
+	"net/http"
 	"sync"
 	"time"
+
+	"github.com/BurntSushi/toml"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/net/proxy"
 )
 
 const (
@@ -17,7 +22,15 @@ const (
 	timeoutNew          = time.Second
 	timeoutWaitAseegnee = time.Second * 2
 	timeoutAssigneed    = time.Second * 5
+
+	configFile = "config.toml"
 )
+
+type config struct {
+	Token   string `toml:"token"`
+	Proxy   string `toml:"proxy"`
+	GroupID int    `toml:"groupid"`
+}
 
 type reviwer struct {
 	ID string
@@ -26,11 +39,12 @@ type reviwer struct {
 type mergeRequestStatus int
 
 type mergeRequest struct {
-	ID        string
-	Status    mergeRequestStatus
-	AddedOn   time.Time
-	UpdatedOn time.Time
-	Assignee  *reviwer
+	ID               string
+	Status           mergeRequestStatus
+	AddedOn          time.Time
+	UpdatedOn        time.Time
+	Assignee         *reviwer
+	ProposeAssignees []reviwer
 }
 
 type engine struct {
@@ -38,6 +52,26 @@ type engine struct {
 	mu            sync.RWMutex
 	mergeRequests map[string]mergeRequest
 	done          chan interface{}
+
+	// new -> waitAssignee
+	onStatusWaitAssignee func(m mergeRequest)
+
+	// waitAssegnee -> assigned
+	onStatusAssigned func(m mergeRequest)
+
+	// waitAssegnee -> waitAssegnee because no on to assign
+	onNoProposedAssignees func(m mergeRequest)
+
+	// assigned -> done
+	onDone func(m mergeRequest)
+
+	// before clean done
+	beforeClean func(m mergeRequest)
+}
+
+func (m *mergeRequest) SetStatus(status mergeRequestStatus) {
+	m.Status = status
+	m.UpdatedOn = time.Now()
 }
 
 func (eng *engine) AddMergeRequest(m mergeRequest) error {
@@ -45,42 +79,86 @@ func (eng *engine) AddMergeRequest(m mergeRequest) error {
 	return nil
 }
 
-func (eng *engine) SetAseegnee(mergeID string, aseegnee *reviwer) error {
-	eng.mu.Lock()
-	defer eng.mu.Unlock()
-
-	m, ok := eng.mergeRequests[mergeID]
-	if !ok {
-		return errors.New("merge request not found")
-	}
-
-	m.Assignee = aseegnee
-	eng.mergeRequests[mergeID] = m
-	return nil
-}
-
 func (eng *engine) Watcher() {
 	for eng.running {
 		eng.mu.Lock()
-		for _, m := range eng.mergeRequests {
+		for id, m := range eng.mergeRequests {
 			timeout := time.Since(m.UpdatedOn)
 
 			switch m.Status {
 			case statusNew:
 				if timeout >= timeoutNew {
+
+					// logging status change
+					m.SetStatus(statusWaitAssignee)
+
+					// TODO: select list of assignees
+
+					m.ProposeAssignees = []reviwer{
+						{
+							ID: "reviwer1",
+						},
+						{
+							ID: "reviwer1",
+						},
+					}
+
+					eng.mergeRequests[id] = m
+					if len(m.ProposeAssignees) > 0 {
+						assignees := ""
+						for i, proposedAssignee := range m.ProposeAssignees {
+							if i != 0 {
+								assignees += ", " + proposedAssignee.ID
+							} else {
+								assignees += proposedAssignee.ID
+							}
+
+						}
+						log.Printf("%q moved to status statusWaitAssignee with proposed assignees %s", m.ID, assignees)
+					} else {
+						log.Printf("%q moved to status statusWaitAssignee without proposed assignees", m.ID)
+					}
+
 					// TODO: message with list of aseegnees
-					m.Status = statusWaitAssignee
+					eng.onStatusWaitAssignee(m)
 				}
 			case statusWaitAssignee:
 				if timeout >= timeoutWaitAseegnee {
-					// TODO: force aseegnee someone
-					m.Status = statusAssigned
+					if len(m.ProposeAssignees) > 0 {
+						m.SetStatus(statusAssigned)
+
+						// TODO: force aseegnee someone
+						m.Assignee = &reviwer{ID: m.ProposeAssignees[0].ID}
+
+						log.Printf("%q moved to status statusAssigned with reviwer %q", m.ID, m.Assignee.ID)
+
+						eng.onStatusAssigned(m)
+					} else {
+						m.SetStatus(statusWaitAssignee)
+						log.Printf("%q keep in status statusWaitAssignee because merge request don't have proposed assignes", m.ID)
+						eng.onNoProposedAssignees(m)
+					}
+
+					eng.mergeRequests[id] = m
 				}
 			case statusAssigned:
 				if timeout >= timeoutAssigneed {
 					// TODO: check merge request status and maybe set done
+					// if mr.Done {
+					//     m.SetStatus(statusDone)
+					// }
+					if false {
+						m.SetStatus(statusDone)
+						eng.mergeRequests[id] = m
+						eng.onDone(m)
+					}
 				}
 			case statusDone:
+				eng.beforeClean(m)
+				// TODO: remove mergerequest
+			default:
+				log.Printf("merge request %q in statusNone state, removing", m.ID)
+				// TODO: remove mergerequest
 			}
 		}
 		eng.mu.Unlock()
@@ -96,6 +174,7 @@ func (eng *engine) Shutdown() error {
 
 	select {
 	case <-time.After(time.Second * 10):
+		return errors.New("timeout")
 	case <-eng.done:
 	}
 
@@ -103,10 +182,73 @@ func (eng *engine) Shutdown() error {
 }
 
 func main() {
+	var cfg config
+	_, err := toml.DecodeFile(configFile, &cfg)
+	if err != nil {
+		log.Fatalf("can not read config %q: %v", configFile, err)
+	}
+
+	var httpClient *http.Client
+
+	if cfg.Proxy != "" {
+		dialer, err := proxy.SOCKS5("tcp", cfg.Proxy, nil, proxy.Direct)
+		if err != nil {
+			log.Fatalf("can't connect to the proxy: %v", err)
+		}
+
+		httpClient = &http.Client{Transport: &http.Transport{Dial: dialer.Dial}}
+	} else {
+		httpClient = &http.Client{}
+	}
+
+	bot, err := tgbotapi.NewBotAPIWithClient(cfg.Token, httpClient)
+	if err != nil {
+		log.Fatalf("can not inittialize telegram bot: %v", err)
+	}
+
+	log.Printf("telegram bot authorized on account %s", bot.Self.UserName)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatalf("telegram can not get update channel: %v", err)
+	}
+
+	for update := range updates {
+		log.Println(update)
+
+		if update.Message == nil || update.Message.From == nil || update.Message.Chat == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("telegam message from %s %v group %s %v", update.Message.From.String(), update.Message.From, update.Message.Chat.Title, update.Message.Chat)
+
+		/*
+			if update.Message == nil { // ignore any non-Message Updates
+				continue
+			}
+
+			log.Printf("[%s] %s",
+			.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		*/
+	}
+
 	eng := engine{
 		running:       true,
 		done:          make(chan interface{}),
 		mergeRequests: map[string]mergeRequest{},
+
+		onStatusWaitAssignee:  func(m mergeRequest) {},
+		onStatusAssigned:      func(m mergeRequest) {},
+		onNoProposedAssignees: func(m mergeRequest) {},
+		onDone:                func(m mergeRequest) {},
+		beforeClean:           func(m mergeRequest) {},
 	}
 
 	go eng.Watcher()
@@ -118,6 +260,17 @@ func main() {
 		UpdatedOn: time.Now(),
 	})
 
+	eng.AddMergeRequest(mergeRequest{
+		ID:        "mr2",
+		Status:    statusNew,
+		AddedOn:   time.Now(),
+		UpdatedOn: time.Now(),
+	})
+
+	time.Sleep(time.Second * 10)
 	log.Println(eng.mergeRequests)
-	eng.Shutdown()
+	err = eng.Shutdown()
+	if err != nil {
+		log.Printf("can not shutdown engine: %v", err)
+	}
 }
